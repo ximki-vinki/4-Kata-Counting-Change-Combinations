baddd2ad461657f0c0b4a5fdab410dfa8b58843c
Манютин <@DESKTOP-7BPDCOU>
2022-06-09 15:48:21 +0300

    Добавьте файлы проекта.

---
+++ 4 Kata Counting Change Combinations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _4_Kata_Counting_Change_Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 4;
+            int[] coins = new int[] { 1, 2 };
+            Console.WriteLine(Kata.CountCombinations(money,coins));
+
+        }
+    }
+
+    public static class Kata
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            return Count(money, ref coins, coins.Length);
+        }
+        private static int Count(int money, ref int[] coins, int count)
+        {
+            if (money < 0 || count < 1 ) return 0;
+            if (money == 0) return 1;
+            return Count(money, ref coins, count - 1) + Count(money - coins[count - 1], ref coins, count);
+
+
+        }
+
+    }
+
+
+}
